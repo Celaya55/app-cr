a40c7c9a4e3d291120128e388e9984854d87ecb5
Juan Carlos <juanccelaya55@gmail.com>
2026-02-17 17:15:35 -0700

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // El framework para crear el servidor
+const { PrismaClient } = require('@prisma/client'); // El cliente para hablar con la DB
+const cors = require('cors'); // Permite que el Frontend se conecte al Backend
+const bcrypt = require('bcryptjs'); // Para encriptar contraseñas
+const jwt = require('jsonwebtoken'); // Para manejar la seguridad (tokens)
+
+const prisma = new PrismaClient();
+const app = express();
+const JWT_SECRET = "mi_clave_secreta"; // Una clave para firmar tus tokens
+
+// Middlewares: Funciones que procesan la info antes de llegar a las rutas
+app.use(cors()); 
+app.use(express.json()); // Permite que tu servidor entienda archivos JSON
